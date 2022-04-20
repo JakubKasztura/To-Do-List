70deec7570322ed51f3dfab1a9a9269d12e60c60
Jakub Kasztura <jakub.kasztura@gmail.com>
2022-04-20 22:35:54 +0200

    Added rendering and counting task list elements

--- todo.js
+++ todo.js
@@ -1,1 +1,31 @@
-// const ;
+const addTaskBtn = document.querySelector(".header__form-btn"),
+  taskList = document.querySelector(".content__todo-list"),
+  taskListArray = [];
+class Task {
+  constructor(text) {
+    this.text = text;
+  }
+  createTask() {
+    const taskElement = document.createElement("li");
+    taskElement.classList.add("content__todo-list-task");
+    taskElement.innerHTML = `${this.text} <span class="lnr lnr-trash"></span>`;
+    taskList.prepend(taskElement);
+    taskListArray.unshift(taskElement);
+  }
+}
+
+const renderList = function (e) {
+  e.preventDefault();
+  let taskInput = document.querySelector(".header__form-input");
+  if (taskInput.value === "") {
+    alert("Insert some task to do");
+    return;
+  }
+  const task = new Task(taskInput.value);
+  task.createTask();
+  let todoCounter = document.querySelector(".content__todo-counter--value");
+  todoCounter.textContent = taskListArray.length;
+  taskInput.value = "";
+  console.log(taskList);
+};
+addTaskBtn.addEventListener("click", renderList);
